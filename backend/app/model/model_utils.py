23d33c8b612c30dfbccec77d18a2a1d135171783
Aishwarya Daga <102050367+aishwaryadaga@users.noreply.github.com>
2024-11-20 16:52:11 +0530

    Update model_utils.py

--- backend/app/model/model_utils.py
+++ backend/app/model/model_utils.py
@@ -2,6 +2,22 @@ from transformers import AutoTokenizer, pipeline
 from app.model.mhp_classifier import MHP_Classifier
 import torch
 import os
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+def remove_stopwords(text):
+    new_list = []
+    words = word_tokenize(text)
+    stopwrds = stopwords.words('english')
+    for word in words:
+        if word not in stopwrds:
+            new_list.append(word)
+    return ' '.join(new_list)
 
 def load_model(config: dict):
     """
@@ -18,6 +34,12 @@ def predict_emotion(model, text: str, tokenizer, emotion_pipeline):
     """
     print("Entering predict emotion")
     # Extract emotion scores
+    text = text.lower()
+    exclude = string.punctuation
+    text = text.translate(str.maketrans('', '', exclude))
+    text = remove_stopwords(text)
+    print("removed stopwords")
+    print(text)
     emotion_scores = emotion_pipeline(text)
     print(emotion_scores)
 
@@ -40,6 +62,7 @@ def predict_emotion(model, text: str, tokenizer, emotion_pipeline):
         )[1]  # Get logits
 
     predicted_class = torch.sigmoid(logits)
+    print(predicted_class)
     predicted_class = torch.round(predicted_class).item()
     print(predicted_class)
 
